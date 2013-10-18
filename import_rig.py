d45cc1e3637d27a5aed5362ffd0be7d49043d9ff
Adhi Hargo <cadmus.sw@gmail.com>
2013-10-18 11:05:30 +0700

    Expose Import Rig macro to UI. Now also usable for plainly-linked rigs.

--- import_rig.py
+++ import_rig.py
@@ -51,12 +51,16 @@ class ADH_LinkAppend(Operator):
     def modal(self, context, event):
         obj = context.active_object
 
-        if obj and obj.type == 'EMPTY' and obj.dupli_group != None:
-            return {'FINISHED'}
+        if obj:
+            if obj.type == 'EMPTY' and obj.dupli_group != None:
+                return {'FINISHED'}
+            else:
+                return {'CANCELLED'}            
 
         return {'PASS_THROUGH'}
 
     def execute(self, context):
+        context.scene.objects.active = None
         bpy.ops.wm.link_append('INVOKE_DEFAULT')
 
         context.window_manager.modal_handler_add(self)
@@ -91,13 +95,25 @@ class ADH_AppendRigScript(Operator):
 
     def execute(self, context):
         obj = context.active_object
-        if not (obj and obj.dupli_group):
+        if not obj:
             return {'CANCELLED'}
 
-        group = obj.dupli_group
+        group = None
+        if obj.type == 'EMPTY' and obj.dupli_group:
+            group = obj.dupli_group
+        elif obj.type == 'ARMATURE' and obj.data.library:
+            for g in obj.data.library.users_id:
+                if type(g) != bpy.types.Group:
+                    continue
+                for o in g.objects:
+                    if o.data == obj.data:
+                        group = g
+                        break
+        if not group:
+            return {'CANCELLED'}
+
         group_libpath = group.library.filepath
         script_list = None
-
         with bpy.data.libraries.load(group_libpath, link=True, relative=True)\
                 as (data_from, data_to):
             script_list = [t for t in data_from.texts if t.endswith('.py')]
@@ -113,10 +129,59 @@ class ADH_AppendRigScript(Operator):
             try:
                 bpy.ops.text.run_script()
             except:
-                self.report({'WARNING'}, 'Unable to run script "%s"' % script)
+                self.report({'WARNING'}, 'Unable to run script "%s".' % script)
         context.space_data.text = prev_text
         context.area.type = prev_type
         
+        return {'FINISHED'}
+
+class ADH_ReloadRig(Operator):
+    "Reload selected armature."
+    bl_idname = 'object.adh_reload_rig'
+    bl_label = 'Reload Rig'
+    bl_options = {'REGISTER', 'UNDO'}
+
+    @classmethod
+    def poll(self, context):
+        obj = context.active_object
+        return obj and obj.type == 'ARMATURE' and obj.data.library != None
+
+    def execute(self, context):
+        rig_obj = context.active_object
+
+        group = None
+        for g in rig_obj.data.library.users_id:
+            if type(g) != bpy.types.Group:
+                continue
+            for o in g.objects:
+                if o.data == rig_obj.data:
+                    group = g
+                    break
+        if group == None:
+            self.report({'ERROR'}, "Rig's group not found.")
+            return {'CANCELLED'}
+
+        group_obj = None
+        for o in context.scene.objects:
+            if o.type == 'EMPTY' and o.dupli_group == group:
+                group_obj = o
+                break
+        if group_obj == None:
+            self.report({'ERROR'}, "Group's instance not found.")
+            return {'CANCELLED'}
+
+        rig_obj.name += "_old.000"
+        bpy.ops.object.mode_set(mode='OBJECT')
+        context.scene.objects.unlink(rig_obj)
+        context.scene.objects.active = group_obj
+        for o in group.objects:
+            if o.type == 'ARMATURE':
+                bpy.ops.object.proxy_make(object = o.name)
+                context.active_object.name = group.name + "_rig"
+                bpy.ops.object.posemode_toggle()
+                break
+            
+        context.scene.update()
         return {'FINISHED'}
 
 class SCENE_PT_adh_scene_panel(bpy.types.Panel):
@@ -131,6 +196,9 @@ class SCENE_PT_adh_scene_panel(bpy.types.Panel):
         column = layout.column(align=True)
         row = column.row()
         row.operator("object.adh_import_rig")
+        row = column.row()
+        row.operator("object.adh_create_rig_proxy")
+        row.operator("object.adh_append_rig_script")
 
 def register():
     bpy.utils.register_module(__name__)
